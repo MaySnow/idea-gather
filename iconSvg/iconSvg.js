f0f2ded675861d0f86acaa839839338c90c422ee
yuxuemei <yuxuemei@aixuexi.com>
2021-10-20 16:15:44 +0800

    feat: init

---
+++ iconSvg/iconSvg.js
@@ -0,0 +1,66 @@
+class IconSvg {
+  constructor(conf) {
+    const { el, url, className } = conf;
+    // 挂载的 dom
+    this.el = el;
+    // 图片地址
+    this.url = url;
+    // inline svg 的 class 名称
+    this.className = className || "svg-class";
+    this.init();
+  }
+  init() {
+    const { url, el, className } = this;
+    this.loadSvg(url, svg => {
+      if(el) {
+        el.innerHTML = svg;
+        const svgElement = el.firstElementChild;
+        // use `viewBox` attribute to get the svg's inherent width and height
+        const viewBox = svgElement.getAttribute('viewBox').split(' ').map(n => Number(n));
+        const widthToHeight = (viewBox[2] / viewBox[3]).toFixed(2);
+        // recursively remove all fill attribute of element and its nested children
+        this.recursivelyRemoveFill(svgElement);
+        // set width and height relative to font size
+        // if growByHeight is true, height set to 1em else width set to 1em and remaining is calculated based on widthToHeight ratio
+        svgElement.setAttribute('height', '1em');
+        svgElement.setAttribute('width', `${widthToHeight}em`);
+        svgElement.classList.add(className);
+
+      }
+    })
+  }
+  /**
+   * 清除 svg 标签内的 fill 样式
+   * @param {*} el 
+   * @returns 
+   */
+  recursivelyRemoveFill(el) {
+    if (!el) {
+        return;
+    }
+    el.removeAttribute('fill');
+    [].forEach.call(el.children, child => {
+        this.recursivelyRemoveFill(child);
+    });
+  }
+
+  /**
+   * 加载 svg
+   * @param {*} url 
+   * @param {*} callback 
+   */
+  loadSvg(url, callback) {
+    const httpRequest = new XMLHttpRequest();
+
+    httpRequest.onreadystatechange = function () {
+      // readyState 4 = complete
+      if (httpRequest.readyState === 4) {
+        // TODO 根据 URL 缓存
+        callback(httpRequest.responseText);
+      }
+    };
+    httpRequest.open('GET', url);
+    httpRequest.send();
+  }
+}
+export default IconSvg;
---
+++ vite.config.js
@@ -0,0 +1,16 @@
+// vite.config.js
+const { resolve } = require('path')
+const { defineConfig } = require('vite')
+const isProduction = process.env.NODE_ENV === "production";
+module.exports = defineConfig({
+  base: isProduction ? "/demo" : "/",
+  build: {
+    outDir: './demo',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        iconSvg: resolve(__dirname, 'iconSvg/index.html')
+      }
+    }
+  }
+})
